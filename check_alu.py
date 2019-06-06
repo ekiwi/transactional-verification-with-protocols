d829bae93870f9d83610fefc24f6620aeef721b4
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-06-06 14:30:51 -0700

    prove subtract

--- check_alu.py
+++ check_alu.py
@@ -5,10 +5,16 @@ import os
 from pysmt.shortcuts import *
 from transactions import *
 
+ALU_RESULT_ADD  = BV(0, 2)
+ALU_RESULT_SR   = BV(1, 2)
+ALU_RESULT_LT   = BV(2, 2)
+ALU_RESULT_BOOL = BV(3, 2)
+
 class AluSpec(Spec):
 	def __init__(self, bits=32):
-		transactions = [self.Add]
-		super().__init__(transactions=[tt(bits) for tt in transactions])
+		self.bits = bits
+		transactions = [self.Add, self.Sub]
+		super().__init__(transactions=[tt() for tt in transactions])
 
 	def BaseProtocol(self, a, b, c, ctrl=None):
 		bits = a.bv_width()
@@ -20,17 +26,21 @@ class AluSpec(Spec):
 			return basic | ((ctrl * bits) >> 1)
 		return basic
 
-	def Op(self, name, BVOperation, ctrl, bits):
-		a = Symbol('a', BVType(bits))
-		b = Symbol('b', BVType(bits))
-		c = Symbol('c', BVType(bits))
+	def Op(self, name, BVOperation, ctrl):
+		a = Symbol('a', BVType(self.bits))
+		b = Symbol('b', BVType(self.bits))
+		c = Symbol('c', BVType(self.bits))
 		semantics = lambda a, b: {'c': BVOperation(a, b)}
 		protocol = self.BaseProtocol(a, b, c, ctrl=ctrl)
-		return Transaction(name=f"{name}<{bits}>", args=[a, b], ret_args=[c], semantics=semantics, proto=protocol)
+		return Transaction(name=f"{name}<{self.bits}>", args=[a, b], ret_args=[c], semantics=semantics, proto=protocol)
 
-	def Add(self, bits):
-		ctrl = Map('i_sub', Bool(False)) | Map('i_rd_sel', BV(0, 2))
-		return self.Op('Add', BVAdd, ctrl, bits)
+	def Add(self):
+		ctrl = Map('i_sub', Bool(False)) | Map('i_rd_sel', ALU_RESULT_ADD)
+		return self.Op('Add', BVAdd, ctrl)
+
+	def Sub(self):
+		ctrl = Map('i_sub', Bool(True)) | Map('i_rd_sel', ALU_RESULT_ADD)
+		return self.Op('Sub', BVSub, ctrl)
 
 src = [os.path.join('serv', 'rtl', name + '.v') for name in ['serv_alu', 'ser_lt', 'ser_shift', 'ser_add', 'shift_reg']]
 
