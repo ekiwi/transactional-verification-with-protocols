cc22cff9c867b9d898a4584866a0e23efbca7f4d
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-06-07 17:15:49 -0700

    alu: try to prove shift by 1 correct ...

--- check_alu.py
+++ check_alu.py
@@ -22,8 +22,9 @@ def flatten(ll: List[list]) -> list:
 class AluSpec(Spec):
 	def __init__(self, bits=32):
 		self.bits = bits
-		transactions = [self.Add, self.Sub, self.Bitwise]
-		super().__init__(transactions=flatten([tt() for tt in transactions]))
+		transactions = [self.Add(), self.Sub(), self.Bitwise(), self.LeftShiftBy(1)]
+		inv = [lambda state: Bool(True)]
+		super().__init__(transactions=flatten(transactions), invariances=inv)
 
 	def BaseProtocol(self, a, b, c, ctrl=None):
 		bits = a.bv_width()
@@ -60,6 +61,32 @@ class AluSpec(Spec):
 		return [self.Op(name, bvop, ctrl = Map('i_rd_sel', ALU_RESULT_BOOL) | Map('i_bool_op', op))
 			for name, bvop, op in ops]
 
+	def LeftShiftBy(self, shift_amount) -> List[Transaction]:
+		assert self.bits == 32
+		assert shift_amount & 0x1f == shift_amount, f"{shift_amount}"
+
+		shift_right = Bool(False)
+		a = Symbol('a', BVType(self.bits))
+		b = BV(shift_amount, 5)
+		c = Symbol('c', BVType(self.bits))
+
+		semantics = lambda a: {'c': BVLShl(a, BVZExt(b, 32-5))}
+
+		start = Map('i_en', Bool(False))
+		# TODO: any constraints on `o_sh_done` ?
+		load_shmat = BitSerial('i_op_b', b)  | (Map('i_init', Bool(True)) | Map('i_en', Bool(True)) | Map('i_shamt_en', Bool(True))  | Map('i_sh_right', shift_right)) * 5
+		# TODO: add padding
+		shift_delay = 32 - shift_amount
+		shift_ctl    = (Map('i_init', Bool(False)) | Map('i_sh_right', shift_right) | Map('i_shamt_en', Bool(False)) | Map('i_rd_sel', ALU_RESULT_SR)) * (32 + shift_delay)
+		shift_en     = Map('i_en', Bool(False)) * shift_delay +  Map('i_en', Bool(True)) * 32
+		shift_arg    = BitSerial('i_buf', a) + BitSerial('i_buf', a, max_bits=shift_delay)
+		shift_done   = Map('o_sh_done', Bool(False)) * shift_delay + Map('o_sh_done', Bool(True)) + Map('o_sh_done', Bool(False)) * 32
+		shift_result = BitSerial('o_rd', c) >> (shift_delay)
+		shift_proto  = shift_arg | shift_done | shift_result | shift_ctl | shift_en
+
+		protocol = start + load_shmat + shift_proto
+		return [Transaction(name=f"Shift<{self.bits}, {shift_amount}>", args=[a], ret_args=[c], semantics=semantics, proto=protocol)]
+
 	def Shifts(self) -> List[Transaction]:
 		assert self.bits == 32
 		print("TODO: SRL & SRA")
@@ -87,7 +114,7 @@ def main() -> int:
 	version = require_yosys()
 	print(f"Using yosys {version}")
 
-	mod = Module.load('serv_alu', src, reset='i_rst')
+	mod = Module.load('serv_alu', src, reset='i_rst', ignore_wires=False)
 	spec = AluSpec()
 
 	reset_env()
