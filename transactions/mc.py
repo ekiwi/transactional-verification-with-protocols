cf8c8d90e5139c65181203fd4b03bb6c351d3949
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-09-27 10:49:16 -0700

    mc: generate some btor

--- transactions/mc.py
+++ transactions/mc.py
@@ -4,7 +4,7 @@
 import subprocess, os
 from typing import Optional
 from pysmt.shortcuts import Symbol, BVType, BV, BVAdd, Not, Equals, get_type
-from pysmt.walkers import TreeWalker
+from pysmt.walkers import DagWalker
 
 from .module import Module, State
 from .engine import Spec, Transaction
@@ -42,6 +42,8 @@ class MCProofEngine:
 			self.solver.comment(f"{tran.name} -> {name}")
 			self.solver.state(sym, next=sym, init=sem_out[name])
 
+		print('\n'.join(self.solver.lines))
+
 	def proof_transactions(self):
 		for trans in self.spec.transactions:
 			self.proof_transaction(trans)
@@ -62,40 +64,41 @@ class BtorMC:
 		self._name_to_ii = {}
 		self._ii_to_sym = {}
 		self.lines = []
+		self.line_cache = {}
 		self.reset()
 
 	def reset(self):
 		rr = parse_yosys_btor(self.header)
 		self.ii = rr['ii'] + 1
-		self._bv_sorts = {s[1]: ii for ii, s in rr['sorts'].items() if s[0] == 'bv'}
-		self._sym_to_ii = {}
+		self._name_to_ii = {}
 		self._ii_to_sym = {}
 		self.lines = []
-
+		self.line_cache = {}
+		# add bv types to line cache
+		for ii, s in rr['sorts'].items():
+			if s[0] == 'bv':
+				self.line_cache[f"sort bitvec {s[1]}"] = ii
 
 	def _l(self, s: str):
-		self.lines.append(f"{self.ii} {s}")
-		ii = self.ii
-		self.ii += 1
-		return ii
+		if s not in self.line_cache:
+			self.lines.append(f"{self.ii} {s}")
+			self.line_cache[s] = self.ii
+			self.ii += 1
+		return self.line_cache[s]
 
 	def _bv(self, bits):
-		if bits not in self._bv_sorts:
-			self._bv_sorts[bits] = self._l(f"sort bitvec {bits}")
-		return self._bv_sorts[bits]
+		return self._l(f"sort bitvec {bits}")
 
 	def _smt2btor(self, formula):
-		converter = Smt2ToBtor2(sym_to_nid=self._sym_to_ii, bv=self._bv, ii=self.ii)
+		converter = Smt2ToBtor2(sym_name_to_nid=self._name_to_ii, line=self._l)
 		converter.walk(formula)
-		self.lines += converter.lines
-		self.ii = converter.ii
 		return self.ii - 1
 
 	def comment(self, s: str):
 		self.lines.append("; " + str(s))
 
 	def state(self, symbol: Symbol, next: Optional = None, init: Optional = None):
-		assert symbol not in self._sym_to_ii, f"symbol {symbol} already exists!"
+		assert symbol.symbol_name() not in self._name_to_ii, f"symbol {symbol} already exists!"
 		typ, name = symbol.symbol_type(), symbol.symbol_name()
 		assert typ.is_bv_type(), f"unsupported type: {typ}"
 		bits = typ.width
@@ -107,8 +110,8 @@ class BtorMC:
 		st = self._l(f"state {sort} {name}")
 		if init is not None:
 			self._l(f"init {sort} {st} {init}")
-		self._name_to_ii[name] = st
 		sym = Symbol(name, BVType(bits))
+		self._name_to_ii[name] = st
 		self._ii_to_sym[st] = sym
 		# next could be referring to state
 		if next is not None:
@@ -125,23 +128,54 @@ class BtorMC:
 		bad = self._l(f"not {self._bv(1)} {good}")
 		return self._l(f"bad {bad}")
 
-class Smt2ToBtor2(TreeWalker):
-	def __init__(self, sym_to_nid: dict, bv, ii: int, env=None):
+class Smt2ToBtor2(DagWalker):
+	def __init__(self, sym_name_to_nid: dict, line, env=None):
+		self.sym_name_to_nid = sym_name_to_nid
+		self._l = line
+		self._register_ops(Smt2ToBtor2.BinOps, self.walk_binop)
+		self._register_ops(Smt2ToBtor2.UnOps, self.walk_unop)
 		super().__init__(env)
-		self.sym_to_nid = sym_to_nid
-		self._bits_to_bv = bv
-		self.lines = []
-		self.ii = ii
 
-	def _sort(self, formula):
-		bits = 1
-		return self._bits_to_bv(bits)
+	def _register_ops(self, ops, foo):
+		for name, op in ops.items():
+			ff = lambda formula, args, **kwargs: foo(op, formula, args, **kwargs)
+			self.__setattr__('walk_' + name,    ff)
+			self.__setattr__('walk_bv_' + name, ff)
 
-	def _l(self, s: str):
-		self.lines.append(f"{self.ii} {s}")
-		ii = self.ii
-		self.ii += 1
-		return ii
+	BinOps = {
+		'iff': 'iff',
+		'implies': 'implies',
+		'equals': 'eq',
+		'add': 'add',
+	}
+	UnOps = {
+		'not': 'not'
+	}
 
-	def walk_bv_constant(self, formula):
-		self._l(f"const {self._sort(formula)} {formula.bv_bin_str()}")
+	def _sort(self, typ):
+		if typ.is_bool_type(): bits = 1
+		elif typ.is_bv_type(): bits = typ.width
+		else: raise RuntimeError(f"unsupported type {typ}")
+		return self._l(f"sort bitvec {bits}")
+
+	def walk_binop(self, op, formula, args, **kwargs):
+		return self._l(f"{op} {self._sort(formula.get_type())} {args[0]} {args[1]}")
+
+	def walk_unop(self, op, formula, args, **kwargs):
+		return self._l(f"{op} {self._sort(formula.get_type())} {args[0]}")
+
+	def walk_bv_zext(self, formula, args, **kwargs):
+		return self._l(f"uext {args[0]} {formula.bv_extend_step()}")
+
+	def walk_bv_extract(self, formula, args, **kwargs):
+		hi = formula.bv_extract_start()
+		lo = formula.bv_extract_end()
+		return self._l(f"slice {args[0]} {hi} {lo}")
+
+	def walk_bv_constant(self, formula, **kwargs):
+		return self._l(f"const {self._sort(formula.get_type())} {formula.bv_bin_str()}")
+
+	def walk_symbol(self, formula, **kwargs):
+		name = formula.symbol_name()
+		assert name in self.sym_name_to_nid, f"unknown symbol {formula}. {list(self.sym_name_to_nid.keys())}"
+		return self.sym_name_to_nid[name]
