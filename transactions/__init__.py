38b91d0d4a1d56590004e04cf319287946e0a463
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-10-02 11:55:48 -0700

    remove old code
    Some of the counter example generation and
    visualization will be resurected later.

--- transactions/__init__.py
+++ transactions/__init__.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from .yosys import require_yosys
-from .solver import Solver
 from .module import Module
 from .spec import Spec, Map, BitSerial, Repeat, Transaction
 from .mc import MCProofEngine
