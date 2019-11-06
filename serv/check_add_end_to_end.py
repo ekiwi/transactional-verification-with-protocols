7582146f0197c836aa568aa0ed0e5c56cc7b63bd
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-11-05 19:09:50 -0800

    serv e2e: port to new format
    TODO: for some reason the SMT2 check completes... Why is that?

--- serv/check_add_end_to_end.py
+++ serv/check_add_end_to_end.py
@@ -6,90 +6,96 @@ from functools import reduce
 from collections import defaultdict
 from pysmt.shortcuts import *
 from transactions import *
-from check_regfile import RegfileSpec
-from check_alu import AluSpec
+from serv.check_regfile import regfile_spec
+from serv.check_alu import alu_spec
+
+regs = Symbol('regs', ArrayType(BVType(5), BVType(32)))
+
+# build instruction
+funct7 = BV(0, 7)
+rs2 = Symbol('rs2', BVType(5))
+rs1 = Symbol('rs1', BVType(5))
+funct3 = BV(0, 3)
+rd = Symbol('rd', BVType(5))
+opcode = BV(0b0110011, 7)
+
+
+instruction = cat(funct7, rs2, rs1, funct3, rd, opcode)
+
+# toplevel spec
+
+# we assume that no interrupt or data bus transaction will be incoming
+always = { 'i_timer_irq': BV(0, 1), 'i_dbus_ack': BV(0,1) }
+protocol = Protocol(
+	[Transition(inputs={'i_ibus_rdt': instruction, 'i_ibus_ack': BV(1,1), **always}, outputs={'o_ibus_cyc': BV(1,1)})] +
+	[Transition(inputs={'i_ibus_ack': BV(0,1), **always}, outputs={'o_ibus_cyc': BV(0,1)})] * 34 +
+	[Transition(inputs={'i_ibus_ack': BV(0,1), **always}, outputs={'o_ibus_cyc': BV(1,1)})]
+)
+
+# semantics of the add instruction
+a = Ite(Equals(rs1, BV(0, 5)), BV(0, 32), Select(regs, rs1))
+b = Ite(Equals(rs2, BV(0, 5)), BV(0, 32), Select(regs, rs2))
+c = BVAdd(a, b)
+regs_n = Ite(Equals(rd, BV(0, 5)), regs, Store(regs, rd, c))
+semantics = {'regs': regs_n}
+
+serv_spec = Spec(state={'regs': regs.symbol_type()}, transactions=[
+	Transaction("Add", args={'rs1': BVType(5), 'rs2': BVType(5), 'rd': BVType(5)}, semantics=semantics, proto=protocol),
+	Transaction("Idle", proto=Protocol([Transition(inputs={'i_ibus_ack': BV(0,1), **always})]))
+])
+
+# common invariances
+other_inv = [
+	Equals(Symbol('decode.state', BVType(2)), BV(0, 2)),
+	Equals(Symbol('decode.cnt', BVType(5)), BV(0, 5)),
+	Equals(Symbol('decode.pending_irq', BVType(1)), BV(0,1)),
+	Equals(Symbol('decode.stage_one_done', BVType(1)), BV(0,1)),
+	Equals(Symbol('decode.o_ctrl_jump', BVType(1)), BV(0,1)),
+	Equals(Symbol('decode.o_cnt_r', BVType(4)), BV(1, 4)),
+	Equals(Symbol('ctrl.en_pc_r', BVType(1)), BV(1,1)),
+]
 
 # experiment to see what happens if we give it a single instruction
+memory = Symbol('regfile.memory', ArrayType(BVType(9), BVType(2)))
+regfile_inv = [
+	conjunction(*[Equals(Select(memory, BV(j, 9)), BV(0,2)) for j in range(16)]),
+	Equals(Symbol('regfile.o_ready', BVType(1)), BV(0,1)),
+	Equals(Symbol('regfile.t', BVType(1)), BV(0,1)),
+	Equals(Symbol('regfile.wcnt', BVType(5)), BV(0,5)),
+]
+e2e_mappings = [
+	StateMapping(
+		arch=Select(regs, BV(ii, 5)),
+		impl=reduce(BVConcat, (Select(memory, BV(ii * 16 + jj, 9)) for jj in reversed(range(16))))
+	)
+for ii in range(1, 32) ]
 
-class ServTop(Spec):
-	def __init__(self, blackboxed=False):
+no_abstraction_check = VerificationProblem(
+	spec=serv_spec,
+	implementation='serv_top',
+	invariances=regfile_inv + other_inv,
+	mappings=e2e_mappings
+)
 
-		########## Architectural State: register file
-
-		if blackboxed:
-			arch_state = {'regs': 'regfile.regs'}
-			mapping = lambda mod: []
-		else:
-			regs = ArrayType(BVType(5), BVType(32))  # ArraySignal('x', 5, 32)
-			arch_state = {'regs': regs}
-			map_regs_to_mem = True
-			def mapping(mod: Module, regs):
-				asserts = []
-				memory = mod['regfile.memory']
-				for ii in range(1, 32):
-					reg = Select(regs, BV(ii, 5))
-					if map_regs_to_mem:
-						iis = [Select(memory, BV(ii * 16 + jj, 9)) for jj in reversed(range(16))]
-						asserts.append(Equals(reg, reduce(BVConcat, iis)))
-					else:
-						for jj in range(16):
-							a = Select(memory, BV(ii * 16 + jj, 9))
-							b = BVExtract(reg, start=jj * 2, end=jj * 2 + 1)
-							asserts.append(Equals(a, b))
-				return asserts
-
-		#### Add Instruction
-		funct7 = BV(0, 7)
-		rs2 = Symbol('spec_rs2', BVType(5))
-		rs1 = Symbol('spec_rs1', BVType(5))
-		funct3 = BV(0, 3)
-		rd = Symbol('spec_rd', BVType(5))
-		opcode = BV(0b0110011, 7)
-
-		instruction = cat(funct7, rs2, rs1, funct3, rd, opcode)
-
-		# protocol
-		first_cycle = Map('i_ibus_rdt', instruction) | Map('i_ibus_ack', Bool(True)) | Map('o_ibus_cyc', Bool(True))
-		middle = Map('i_ibus_ack', Bool(False)) | Map('o_ibus_cyc', Bool(False))
-		last_cycle = Map('i_ibus_ack', Bool(False)) | Map('o_ibus_cyc', Bool(True))
-		always = Map('i_timer_irq', Bool(False)) | Map('i_dbus_ack', Bool(False))
-
-		protocol = (first_cycle + (middle * 34) + last_cycle) | (always * 36)
-		assert len(protocol) == 36
-
-
-		def semantics(spec_rs1, spec_rs2, spec_rd, regs):
-			a = Ite(Equals(spec_rs1, BV(0, 5)), BV(0,32), Select(regs, spec_rs1))
-			b = Ite(Equals(spec_rs2, BV(0, 5)), BV(0,32), Select(regs, spec_rs2))
-			c = BVAdd(a, b)
-			regs_n = Ite(Equals(spec_rd, BV(0, 5)), regs, Store(regs, spec_rd, c))
-			return {'regs': regs_n}
-
-		# TODO: remove regfile invariances (they aren't needed for modular verification)
-		# def x0_inv(state):
-		# 	m = state['regfile.memory']
-		# 	return conjunction(*[Equals(Select(m, BV(j, 9)), BV(0,2)) for j in range(16)])
-		inv = [
-			#lambda state: Iff(state['regfile.o_ready'], Bool(False)),
-			#lambda state: Iff(state['regfile.t'],Bool(False)),
-			lambda state: Equals(state['decode.state'], BV(0, 2)),
-			lambda state: Equals(state['decode.cnt'], BV(0, 5)),
-			lambda state: Iff(state['decode.pending_irq'], Bool(False)),
-			lambda state: Iff(state['decode.stage_one_done'], Bool(False)),
-			lambda state: Iff(state['decode.o_ctrl_jump'], Bool(False)),
-			lambda state: Equals(state['decode.o_cnt_r'], BV(1, 4)),
-			lambda state: Iff(state['ctrl.en_pc_r'], Bool(True)),
-			#lambda state: Equals(state['regfile.wcnt'], BV(0, 5)),
-			#x0_inv
-		]
-
-		transactions = [Transaction(name=f"e2e_add", args=[rs1, rs2, rd], ret_args=[], semantics=semantics, proto=protocol)]
-
-		super().__init__(transactions=transactions, arch_state=arch_state, mapping=mapping, invariances=inv)
+# the other possibility is to replace the alu and the regfile with their spec when checking
+abs_mappings = [StateMapping(arch=regs, impl=Symbol('regfile.regs', regs.symbol_type()))]
+abstract_refile_and_alu_check = VerificationProblem(
+	spec=serv_spec,
+	implementation='serv_top',
+	invariances=other_inv,
+	mappings=abs_mappings,
+	submodules={'regfile': regfile_spec, 'alu': alu_spec}
+)
 
 src = [os.path.join('fork', 'rtl', name + '.v') for name in ['serv_alu', 'ser_lt', 'ser_shift', 'ser_add', 'shift_reg', 'serv_bufreg', 'serv_csr', 'serv_ctrl', 'serv_decode', 'serv_regfile', 'serv_mem_if', 'serv_top']]
 
-def blackbox(spec: ServTop, disable: bool):
+def get_transaction(spec: Spec, name: str) -> Transaction:
+	try:
+		return next(tt for tt in spec.transactions if tt.name == name)
+	except StopIteration:
+		assert False, f"Failed to find transaction {name}. {[tt.name for tt in spec.transactions]}"
+
+def blackbox(disable: bool):
 	if disable: return [], None
 
 	blackbox = []
@@ -97,30 +103,27 @@ def blackbox(spec: ServTop, disable: bool):
 
 	def register(typ, instance, trace, spec):
 		blackbox.append(typ)
-		transaction_traces['e2e_add'][instance] = {'spec': spec, 'trace': [spec.get_transaction(name) for name in trace]}
+		transaction_traces['e2e_add'][instance] = {'spec': spec, 'trace': [get_transaction(spec, name) for name in trace]}
 
 	# for now magically assume that we know the correct transaction traces
-	register(typ='serv_regfile', instance='regfile', trace=['rw', 'idle'], spec=RegfileSpec())
-	register(typ='serv_alu', instance='alu', trace=['idle', 'idle', 'Add<32>', 'idle'], spec=AluSpec())
+	register(typ='serv_regfile', instance='regfile', trace=['RW', 'Idle'], spec=regfile_spec)
+	register(typ='serv_alu', instance='alu', trace=['Idle', 'Idle', 'Add', 'Idle'], spec=alu_spec)
 
 	return blackbox, transaction_traces
 
 
 def main() -> int:
-	version = require_yosys()
-	print(f"Using yosys {version}")
 
-	spec = ServTop(blackboxed=True)
-	blackboxes, transaction_traces = blackbox(spec, disable=False)
+	blackboxes, transaction_traces = blackbox(disable=True)
 	mod = Module.load('serv_top', src, reset=HighActiveReset('i_rst'), ignore_wires=False, blackbox=blackboxes)
 
 
 	print(f"Trying to proof {mod.name}")
-	#print(mod)
+
 	ee = SMT2ProofEngine(outdir='../smt2')
 	#ee = MCProofEngine(outdir="../btor2")
-	veri = Verifier(mod, spec, ee)
-	veri.proof_all(transaction_traces=transaction_traces)
+	veri = Verifier(mod, no_abstraction_check, ee)
+	veri.proof_all()
 
 	return 0
 
