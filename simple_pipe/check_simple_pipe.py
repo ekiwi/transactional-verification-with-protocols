c87b80b5972001c03d436a14fd2290bafc5bb97a
noloerino <jhshi07@gmail.com>
2019-11-08 14:26:36 -0800

    Copy mappings from ref-rel-var-map

--- simple_pipe/check_simple_pipe.py
+++ simple_pipe/check_simple_pipe.py
@@ -7,17 +7,17 @@ from transactions import *
 
 class SimplePipeSpec(Spec):
     INSTR_BIT_WIDTH = 8
-    NOPCODE = BVZero(2)
+    NOP_INSTR = BV(0, INSTR_BIT_WIDTH)
 
     def __init__(self):
         # In this simple example, we can just insert nops between instructions for now
         inst_wire = Symbol("spec_inst", BVType(SimplePipeSpec.INSTR_BIT_WIDTH))
-        dummy_rf_data_wire = Symbol("spec_dummy_rf_data", BVType(SimplePipeSpec.INSTR_BIT_WIDTH))
-        dummy_read_rf_wire = Symbol("spec_dummy_read_rf", BVType(2))
         protocol = Protocol(
-            # TODO insert nops somehow
             [
-                {"inst": inst_wire, "dummy_read_rf": dummy_read_rf_wire, "dummy_rf_data": dummy_rf_data_wire}
+                {"inst": inst_wire},
+                # Insert nops to simulate pipeline flush
+                {"inst": SimplePipeSpec.NOP_INSTR},
+                {"inst": SimplePipeSpec.NOP_INSTR},
             ]
         )
         arch_state = {
@@ -27,10 +27,29 @@ class SimplePipeSpec(Spec):
             "r3": BVType(SimplePipeSpec.INSTR_BIT_WIDTH),
         }
         def mapping(mod: Module, r0, r1, r2, r3):
+            # Corresponds to ref-rel-var-map.json
             return [
+                Or(Not(Equals(mod["reg_0_w_stage"], BV(0b10, 2))), Equals(mod["ex_alu_result"], r0)),
+                Or(Not(Equals(mod["reg_0_w_stage"], BV(0b11, 2))), Equals(mod["ex_alu_result"], r0)),
+                Or(Not(Equals(mod["reg_0_w_stage"], BV(0b01, 2))), Equals(mod["ex_wb_val"], r0)),
+                Or(Not(Equals(mod["reg_0_w_stage"], BV(0b00, 2))), Equals(mod["registers[0]"], r0)),
 
+                Or(Not(Equals(mod["reg_1_w_stage"], BV(0b10, 2))), Equals(mod["ex_alu_result"], r1)),
+                Or(Not(Equals(mod["reg_1_w_stage"], BV(0b11, 2))), Equals(mod["ex_alu_result"], r1)),
+                Or(Not(Equals(mod["reg_1_w_stage"], BV(0b01, 2))), Equals(mod["ex_wb_val"], r1)),
+                Or(Not(Equals(mod["reg_1_w_stage"], BV(0b00, 2))), Equals(mod["registers[1]"], r1)),
+
+                Or(Not(Equals(mod["reg_2_w_stage"], BV(0b10, 2))), Equals(mod["ex_alu_result"], r2)),
+                Or(Not(Equals(mod["reg_2_w_stage"], BV(0b11, 2))), Equals(mod["ex_alu_result"], r2)),
+                Or(Not(Equals(mod["reg_2_w_stage"], BV(0b01, 2))), Equals(mod["ex_wb_val"], r2)),
+                Or(Not(Equals(mod["reg_2_w_stage"], BV(0b00, 2))), Equals(mod["registers[2]"], r2)),
+
+                Or(Not(Equals(mod["reg_3_w_stage"], BV(0b10, 2))), Equals(mod["ex_alu_result"], r3)),
+                Or(Not(Equals(mod["reg_3_w_stage"], BV(0b11, 2))), Equals(mod["ex_alu_result"], r3)),
+                Or(Not(Equals(mod["reg_3_w_stage"], BV(0b01, 2))), Equals(mod["ex_wb_val"], r3)),
+                Or(Not(Equals(mod["reg_3_w_stage"], BV(0b00, 2))), Equals(mod["registers[3]"], r3)),
             ]
-        def semantics(inst, dummy_read_rf, r0, r1, r2, r3):
+        def semantics(inst, r0, r1, r2, r3):
             # Perhaps it would be better to specify an obj of input dicts + arch state or something?
             opcode = BVExtract(inst, 6, 7)
             rs1 = BVExtract(inst, 4, 5)
@@ -69,13 +88,13 @@ class SimplePipeSpec(Spec):
                 "r2": ...,
                 "r3": ...,
             }
-        transactions = [Transaction(name="register", args=[in_wire], ret_args=[out_wire], semantics=semantics, proto=protocol)]
+        transactions = [Transaction(name="simple_pipe", args=[inst_wire], ret_args=[], semantics=semantics, proto=protocol)]
         super().__init__(arch_state=arch_state, mapping=mapping, transactions=transactions)
 
-def prove(verilog_path, spec_class, reset="rst"):
+def prove(verilog_path, mod_name, spec_class, reset="rst"):
     version = require_yosys()
     print(f"Using yosys {version}")
-    mod = Module.load("register", [verilog_path], reset=reset)
+    mod = Module.load(mod_name, [verilog_path], reset=reset)
     spec = spec_class()
     print(f"Trying to prove {mod.name}")
     ee = SMT2ProofEngine(outdir='smt2')
@@ -85,8 +104,8 @@ def prove(verilog_path, spec_class, reset="rst"):
 def main():
     correct_v = os.path.join("simple_pipe.v")
     wrong_v = os.path.join("simple_pipe_wrong.v")
-    prove(correct_v, SimplePipeSpec)
-    prove(wrong_v, SimplePipeSpec)
+    prove(correct_v, "pipeline_v", SimplePipeSpec)
+    prove(wrong_v, "pipeline_v", SimplePipeSpec)
 
 if __name__ == '__main__':
     main()
