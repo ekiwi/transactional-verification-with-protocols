c313c3e0a6d9ace44c1ecab26bca61283978a29e
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-11-12 10:51:36 -0800

    verifier: declar inputs together with conditions

--- transactions/verifier.py
+++ transactions/verifier.py
@@ -183,10 +183,12 @@ class Verifier:
 			for subtran in trace[name]:
 				assert subtran in spec.transactions, f"Subtransaction {subtran.name} is not part of the spec for {name}"
 
-	def generate_input_conditions(self, tran: Transaction, submodule: RtlModule, check: BoundedCheck, offset: int = 0):
+	def generate_inputs(self, tran: Transaction, submodule: RtlModule, check: BoundedCheck, offset: int = 0, prefix: str = "", assume_dont_assert_requirements: bool = False):
 		var_finder = FindVariableIntervals()
+
 		# variable -> interval -> (cycle, signal_expr)
 		var2inputs: Dict[SmtExpr, Dict[Tuple[int, int], List[Tuple[int, SmtExpr]]]] = defaultdict(lambda: defaultdict(list))
+
 		# find constant and variable mapping on the protocol inputs
 		for ii, tt in enumerate(tran.proto.transitions):
 			for signal_name, expr in tt.inputs.items():
@@ -199,30 +201,59 @@ class Verifier:
 					else:
 						assert var_lsb == 0 and var_msb+1 == var.get_type().width, f"Expect constants to be simplified!"
 						# check that the input has the correct constant value in cycle ii+offset
-						check.assert_at(ii+offset, Equals(sig_expr, var))
+						if assume_dont_assert_requirements:
+							check.assume_at(ii + offset, Equals(sig_expr, var))
+						else:
+							check.assert_at(ii+offset, Equals(sig_expr, var))
 
-		# find protocol inputs that refer to the same bits in the transaction inputs
+		# make sure that all arguments of the transaction are defined in the protocol
+		for name, tpe in tran.args.items():
+			if Symbol(name, tpe) not in var2inputs:
+				print(f"WARN: in transaction {tran.name}: argument {name} is not defined by the protocol. Will be random!")
+				check.constant(Symbol(prefix+name, tpe))
+
+		# declare protocol arguments and their restrictions
 		for var, refs in var2inputs.items():
-			# check that there are not overlapping intervals as they aren't supported yet
+			# check that there are no overlapping intervals as they aren't supported yet
 			covered_bits = set()
 			for (msb, lsb) in refs.keys():
 				for bit in range(lsb, msb+1):
 					assert bit not in covered_bits, f"Overlapping intervals on {var}[{bit}]"
 					covered_bits.add(bit)
+
+			# check that all bits are defined
+			for bit in range(var.symbol_type().width):
+				if bit not in covered_bits:
+					print(f"WARN: in transaction {tran.name}: argument bit {var}[{bit}] is not defined by the protocol.")
+
+			# generate input constant
+			var_sym = Symbol(prefix+var.symbol_name(), var.symbol_type())
+			check.constant(var_sym)
+
 			# generate conditions for each interval
 			for ((msb, lsb), mappings) in refs.items():
-				# if there is only a single mapping for this interval of var, then the value is unrestricted!
-				if len(mappings) <= 1: continue
+				assert len(mappings) > 0
+				full_range = msb + 1 == var.symbol_type().width and lsb == 0
 
 				# find (one of) the first references to this interval
 				mappings_sorted_by_cycle = sorted(mappings, key=lambda ii: ii[0])
 				start = mappings_sorted_by_cycle[0]
+
 				# we bind the start mapping to a constant and then check every other mapping against the constant
-				constant = Symbol(f"{var.symbol_name()}[{msb}:{lsb}]", BVType(msb-lsb+1))
-				check.constant(constant)
+				if full_range:
+					constant = var_sym
+				else:
+					constant = Symbol(f"{var.symbol_name()}[{msb}:{lsb}]", BVType(msb-lsb+1))
+					check.constant(constant)
+					check.assume_always(Equals(constant, BVExtract(var_sym, start=lsb, end=msb)))
+
+				# map the constant to the input variable symbol
 				check.assume_at(start[0], Equals(constant, start[1]))
 				for cycle, expr in mappings_sorted_by_cycle[1:]:
-					check.assert_at(cycle, Equals(expr, constant))
+					if assume_dont_assert_requirements:
+						check.assume_at(cycle, Equals(expr, constant))
+					else:
+						check.assert_at(cycle, Equals(expr, constant))
 
 
 	def verify_transaction_trace(self, tran: Transaction, traces: Dict[str, List[Transaction]]):
@@ -240,7 +271,7 @@ class Verifier:
 			for instance, trace in  traces.items():
 				offsets = [0] + list(itertools.accumulate(transaction_len(tt) for tt in trace))
 				for ii, (start_cycle, tran) in enumerate(zip(offsets, trace)):
-					self.generate_input_conditions(tran, submodule=self.mod.submodules[instance], check=check, offset=start_cycle)
+					self.generate_inputs(tran, submodule=self.mod.submodules[instance], check=check, offset=start_cycle)
 				# TODO:  check our assumption that the submodule `instance` is never reset
 
 		print("WARN: transaction traces are currently NOT fully verified! FIXME!")
