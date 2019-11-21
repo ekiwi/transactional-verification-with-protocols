6b0e4b04941f8749242eaffd3921d791ba60bca3
noloerino <jhshi07@gmail.com>
2019-11-21 02:01:59 -0800

    Add clock pd to shift tb

--- serv/testbench/test_shift.py
+++ serv/testbench/test_shift.py
@@ -2,15 +2,17 @@
 Tests serv's shift instructions by applying appropriate inputs to the ALU module.
 """
 import cocotb
+from cocotb.clock import Clock
 from cocotb.triggers import Timer
 
-@cocotb.test()
+PERIOD = 62 # derived by examining waveform
+TIMESTEP = "ns" # fs/ps/ns/us/ms/sec/None
+
 def test_sll(dut):
     log = dut._log
-    log.info("Running test!")
-    for cycle in range(10):
-        dut.clk = 0
-        yield Timer(1, units='ns')
-        dut.clk = 1
-        yield Timer(1, units='ns')
-    log.info("Running test!")
+    log.info("Running test")
+    # In an sll (as demonstrated by instructions 28 and 7C of I-SLL-01.fst), o_rd is held 0 for the duration of the
+    # instruction's execution, while o_sh_done is asserted after the passage of 63 cycles after i_en is first raised
+    # (presumably in the manner of a stall). o_rd will then mirror the value of the shifter's o_q.
+    cocotb.fork(Clock(dut.clk, PERIOD, TIMESTEP).start())
+
