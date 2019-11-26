d42851370dfcfff969a8a6833c6f61c9022e7426
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-11-25 17:41:49 -0800

    multi: add data dependent delay example

---
+++ examples/multi_data.v
@@ -0,0 +1,32 @@
+// latency depends on data, no done signal
+module multi_data(
+	input wire clock,
+	input wire reset,
+	input wire start,
+	input wire [31:0] in,
+	output wire done,
+	output wire [31:0] out);
+
+reg [31:0] buffer;
+always @(posedge clock)
+	if(start) buffer <= in;
+
+reg [1:0] delay;
+always @(posedge clock)
+	if(start) delay <= in[1:0];
+
+reg [3:0] counter;
+always @(posedge clock)
+	if(start) counter <= 4'h0;
+	else      counter <= counter + 4'h1;
+
+reg running;
+always @(posedge clock)
+	if(reset)      running <= 1'h0;
+	else if(start) running <= 1'h1;
+	else if(done)  running <= 1'h0;
+
+assign done = running & (counter == {2'h0, delay});
+assign out = (done)? buffer : 32'h0;
+
+endmodule
