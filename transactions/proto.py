b223cece95901734f6d8a90b8a45b7dd16ac40c6
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-12-04 15:54:33 -0800

    proto: remove some old, dead code

--- transactions/proto.py
+++ transactions/proto.py
@@ -7,70 +7,6 @@ from collections import defaultdict
 from typing import Set, Union, Iterator
 import copy
 
-def make_symbols(symbols: Dict[str, SmtSort], prefix: str = "", suffix: str = "") -> Dict[str, Symbol]:
-	return {name: Symbol(prefix + name + suffix, tpe) for name, tpe in symbols.items()}
-
-# protocol processing algorithms
-
-@dataclass
-class Range:
-	msb: int
-	lsb: int
-
-@dataclass
-class Signal:
-	name: str
-	range: Optional[Range] = None
-	def __str__(self):
-		if self.range is None: return self.name
-		return f"{self.name}[{self.range.msb}:{self.range.lsb}]"
-
-@dataclass
-class Constraint:
-	signal: Signal
-
-@dataclass
-class ConstConstraint(Constraint):
-	value: int
-	def __str__(self): return f"{self.signal} = {self.value}"
-	def __repr__(self): return str(self)
-
-@dataclass
-class EquivalenceConstraint(Constraint):
-	transition: int
-	other: Signal
-
-@dataclass
-class ProtocolGraph:
-	pass
-
-@dataclass
-class ProtocolGraphTransition:
-	inputs: Dict[str, List[Constraint]]
-	outputs: Dict[str, List[Constraint]]
-	guard: SmtExpr
-
-@dataclass
-class ProtocolGraphEdge:
-	guards: List[SmtExpr]
-	input_constraints: SmtExpr
-	input_mappings: Dict[str, ]
-	inputs: Dict[str, SmtExpr]
-	outputs: Dict[str, SmtExpr]
-	transactions: Set[Transaction]
-
-@dataclass
-class ProtocolGraphState:
-	next: List[ProtocolGraphTransition]
-
-ProtocolPath = List[ProtocolGraphTransition]
-
-
-
-
-
-
-
 ##### Verification Protocol
 @dataclass
 class VeriPath:
@@ -90,9 +26,6 @@ class VeriSpec:
 	paths: List[VeriPath]
 	edges: List[VeriEdge]
 
-
-# variable -> interval -> (cycle, signal_expr)
-Var2InputType = Dict[SmtExpr, Dict[Tuple[int, int], List[Tuple[int, Signal]]]]
 
 def extract_if_not_redundant(expr: SmtExpr, msb: int, lsb: int) -> SmtExpr:
 	assert expr.get_type().is_bv_type(), f"{expr} : {expr.get_type()}"
@@ -184,7 +117,7 @@ def to_verification_graph(proto: Protocol, tran: Transaction, io_prefix: str) ->
 
 
 
-###################### New Types
+###################### Protocol Builder Code
 @dataclass
 class DontCareClass:
 	pass
@@ -328,125 +261,6 @@ class ProtocolBuilder:
 		self.step()
 		self._active = False
 		return Protocol(self._start)
-
-class ProtocolMerger:
-	def __init__(self, tran0: Transaction, tran1: Transaction):
-		self.args = make_symbols(tran.args)
-		self.ret_args = make_symbols(tran.ret_args)
-
-	def merge(self, s0: ProtocolGraphState, s1: ProtocolGraphState) -> ProtocolGraphState:
-		
-		for t0 in s0.next:
-			for t1 in s1.next:
-				if input_different(t0, t1):
-					continue
-				if output_compatible(t0, t1):
-					merge(t0, t1)
-					break
-				# there exists a common input trace, but outputs are not compatible
-				# => if this is between two different transactions -> abort!
-				# => if this is for the same transaction -> new edge
-
-
-def merge_protocols(s0: ProtocolGraphState, s1: ProtocolGraphState) -> ProtocolGraphState:
-	pass
-
-def compare_transitions(t1: Transition, t2: Transition):
-	inputs = set(t1.inputs.keys()) | set(t2.inputs.keys())
-	for ii in inputs:
-		# unconstrained in one example
-		if ii not in t1.inputs: print(f"{ii} missing from t1")
-		if ii not in t2.inputs: print(f"{ii} missing from t2")
-		# load expressions
-		e1, e2 = t1.inputs[ii], t2.inputs[ii]
-		if e1 == e2: print(f"{ii} is the same in t1 and t2")
-		# check for constant constraint
-
-def is_unsat(expr: SmtExpr) -> bool:
-	return False
-
-
-
-def get_input_constraints(tt: ProtocolGraphTransition) -> SmtExpr:
-	return True
-
-def visit_edges(prefix: SmtExpr, e0: ProtocolGraphEdge, e1: ProtocolGraphEdge) -> List[ProtocolGraphEdge]:
-	# check if there is an input that satisfies both edges
-	input_constraints = And(prefix, And(e0.input_constraints, e1.input_constraints))
-	common_input_exists = not is_unsat(input_constraints)
-
-	# if the edges are mutually exclusive, we stop merging
-	if not common_input_exists:
-		return [e0, e1]
-
-	# check to see if the two edges refer to the same transaction
-	same_transaction = len(e0.transactions) == len(e1.transactions) == 1 and e0.transactions[0] == e1.transactions[0]
-
-	# check if the outputs are compatible
-
-def protocol_constraints(proto: LegacyProtocol):
-	""" compute i/o constraints of protocol (without semantics) """
-
-	# NOTE: this replaces parts of `generate_inputs` in `verifier.py`
-
-	# variable -> interval -> (cycle, signal_expr)
-	var2inputs: Dict[SmtExpr, Dict[Tuple[int, int], List[Tuple[int, Signal]]]] = defaultdict(lambda: defaultdict(list))
-
-	input_constraints = [defaultdict(list) for _ in range(len(proto.transitions))]
-
-	# find constant and variable mapping on the protocol inputs
-	for ii, tt in enumerate(proto.transitions):
-		for signal_name, expr in tt.inputs.items():
-			expr_width = expr.get_type().width
-			for (signal_msb, signal_lsb, (var_msb, var_lsb, var)) in FindVariableIntervals.find(expr):
-				is_full = signal_lsb == 0 and signal_msb + 1 == expr_width
-				if is_full:
-					sig_expr = Signal(name = signal_name)
-				else:
-					sig_expr = Signal(name = signal_name, range=Range(lsb=signal_lsb, msb=signal_msb))
-				if var.is_symbol():
-					var2inputs[var][(var_msb, var_lsb)].append((ii, sig_expr))
-				else:
-					assert var_lsb == 0 and var_msb + 1 == var.get_type().width, f"Expect constants to be simplified!"
-					value = int(var.bv_str(), 2)
-					input_constraints[ii][signal_name].append(ConstConstraint(signal=sig_expr, value=value))
-
-	for var, refs in var2inputs.items():
-		# check that there are no overlapping intervals as they aren't supported yet
-		covered_bits = set()
-		for (msb, lsb) in refs.keys():
-			for bit in range(lsb, msb + 1):
-				assert bit not in covered_bits, f"Overlapping intervals on {var}[{bit}]"
-				covered_bits.add(bit)
-
-		# check that all bits are defined
-		for bit in range(var.symbol_type().width):
-			if bit not in covered_bits:
-				print(f"WARN:  argument bit {var}[{bit}] is not defined by the protocol.")
-
-		# generate conditions for each interval
-		for ((msb, lsb), mappings) in refs.items():
-			assert len(mappings) > 0
-			# if a variable is only referenced once, then this imposes no constraints
-			if len(mappings) == 0: continue
-			full_range = msb + 1 == var.symbol_type().width and lsb == 0
-
-			# find (one of) the first references to this interval
-			mappings_sorted_by_cycle = sorted(mappings, key=lambda ii: ii[0])
-			start = mappings_sorted_by_cycle[0]
-
-			# we bind the start mapping to a constant and then check every other mapping against the constant
-			transition = start[0]
-			if full_range:	const = Signal(start[1])
-			else:			const = Signal(start[1], Range(lsb=lsb, msb=msb))
-
-			# create constraint for every signal/cycle
-			for cycle, expr in mappings_sorted_by_cycle[1:]:
-				input_constraints[cycle][expr.name] = EquivalenceConstraint(signal=expr, transition=transition, other=const)
-
-	print(input_constraints)
-
-
 
 from pysmt.walkers import DagWalker
 
