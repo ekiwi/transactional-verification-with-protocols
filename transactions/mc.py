745083ac8b7021f8fd1080386398b78105668954
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-12-05 16:12:23 -0800

    verifier: keep reset inactive --> multi01 verifies!

--- transactions/mc.py
+++ transactions/mc.py
@@ -304,6 +304,7 @@ class Smt2ToBtor2(DagWalker):
 	def walk_bv_add(self, formula, args, **kwargs): return self.walk_binop("add", formula, args, **kwargs)
 	def walk_bv_sub(self, formula, args, **kwargs): return self.walk_binop("sub", formula, args, **kwargs)
 	def walk_bv_or(self, formula, args, **kwargs): return self.walk_binop("or", formula, args, **kwargs)
+	def walk_or(self, formula, args, **kwargs): return self.walk_binop("or", formula, args, **kwargs)
 	def walk_bv_and(self, formula, args, **kwargs): return self.walk_binop("and", formula, args, **kwargs)
 	def walk_and(self, formula, args, **kwargs): return self.walk_binop("and", formula, args, **kwargs)
 	def walk_bv_xor(self, formula, args, **kwargs): return self.walk_binop("xor", formula, args, **kwargs)
