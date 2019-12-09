a07e5981891c1d7233944b7b46f03e84d44fac27
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-12-08 18:24:46 -0800

    examples: remove done signal from multi2const

--- examples/multi2const.v
+++ examples/multi2const.v
@@ -3,18 +3,13 @@ module multi2const(
 	input wire clock,
 	input wire reset,
 	input wire start,
-
-	output wire lsb_done,
-	output wire msb_done,
-
 	input wire [63:0] inp,
-	output wire done,
+//	output wire done,
 	output wire [63:0] out);
 
 
-//wire lsb_done, msb_done;
+wire lsb_done, msb_done;
 wire [31:0] lsb_out, msb_out;
-
 
 multi0 lsb_unit(
 	.clock(clock), .reset(reset), .start(start),
@@ -29,19 +24,20 @@ reg [31:0] msb_buffer, lsb_buffer;
 always @(posedge clock) if(msb_done) msb_buffer <= msb_out;
 always @(posedge clock) if(lsb_done) lsb_buffer <= lsb_out;
 
-reg [3:0] counter;
-always @(posedge clock)
-	if(start) counter <= 4'h0;
-	else      counter <= counter + 4'h1;
+// since this unit has predictable timing, no "done" signal is needed
+//reg [3:0] counter;
+//always @(posedge clock)
+//	if(start) counter <= 4'h0;
+//	else      counter <= counter + 4'h1;
+//
+//reg running;
+//always @(posedge clock)
+//	if(reset)      running <= 1'h0;
+//	else if(start) running <= 1'h1;
+//	else if(done)  running <= 1'h0;
 
-reg running;
-always @(posedge clock)
-	if(reset)      running <= 1'h0;
-	else if(start) running <= 1'h1;
-	else if(done)  running <= 1'h0;
 
-
-assign done = (counter == 4'h3) & running;
+//assign done = (counter == 4'h3) & running;
 assign out  =
 	( msb_done &  lsb_done)? {msb_out, lsb_out} :
 	( msb_done & !lsb_done)? {msb_out, lsb_buffer} :
